f7a61e6c67d5bde35951465aa62bd646c5a60201
olhapatsyienko <20107984@mail.wit.ie>
2025-12-17 20:00:12 +0000

    Added userId to tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -10,6 +10,21 @@ router.get('/', async (req, res) => {
     res.status(200).json(tasks);
 });
 
+// Get all tasks
+router.get('/', async (req, res) => {
+    const tasks = await Task.find().populate('userId', 'username');
+    res.status(200).json(tasks);
+});
+
+
+// Get a user's tasks
+router.get('/user/:uid', async (req, res) => {
+    const tasks = await Task.find({ userId: `${req.params.uid}`});
+    res.status(200).json(tasks);
+});
+
+
+
 // create a task
 router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
